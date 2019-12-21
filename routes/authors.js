71e529de898ea3ca974552d1bbf043428009a404
Gourav Khator <gouravkhator9@gmail.com>
2019-12-21 10:59:22 +0530

    Initial Author Routes

---
+++ routes/authors.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const Author = require('../models/author');
+const router = express.Router();
+
+//all authors
+router.get('/', async (req, res) => {
+    let searchOptions = {};
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i'); //i for case insensitive
+        //so it searches for all possible expressions
+    } //if nothing sent then all authors arefetched
+    try {
+        const authors = await Author.find(searchOptions); //finding authors with some search conditions
+        //fetching all of them
+        res.render('authors/index', { authors, searchOptions: req.query });
+    } catch{
+        res.redirect('/');
+    }
+
+});
+
+//new author
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new Author() });
+});
+
+//Create author route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    });
+
+    try {
+        const newAuthor = await author.save(); //after saving in db it returns that newAuthor with _id
+        //res.redirect(`authors/${newAuthor.id}`);
+        res.redirect(`authors/`);
+    } catch{
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating author'
+        });
+    }
+    //This code could be done normally but would use nested callbacks so async await is used
+    //as author.save() is asynchronous so newAuthor will be populated only when it finishes its callback
+});
+module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 if (process.env.NODE_ENV !== 'production') {
     //if the app is not in production  
     //then mongodb will be in localhost and the path is in .env file
-    require('dotenv').parse();
+    require('dotenv').config({ path: './.env' });
 }
 
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
-const indexRouter = require('./routes');
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout'); //to include header and footer in html to reuse
 app.use(expressLayouts);
 app.use(express.static('public')); //public folder for css and js files
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }); //mongoose uses old style of accessing database 
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }); //mongoose uses old style of accessing database 
 //so using new would be better
 //to load environment variables in app we use dotenv 
 const db = mongoose.connection;
@@ -25,5 +28,14 @@ db.on('error', err => console.error(err));
 db.once('open', () => console.log('Connected to mongoose'));
 
 app.use('/', indexRouter); // '/' is the root for indexRouter
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000); //process.env.PORT which is used in hosting the website
+
+
+//dependencies :
+/* body-parser to get values from form fields
+_form_fields has some reusable form fields to be used in different pages
+views has authors folder , layouts, partials
+partials for reusable headers in diff. pages
+*/
