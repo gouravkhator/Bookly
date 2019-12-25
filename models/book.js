00ab18dbae4c7e404dfe7175a079088ac28d41b4
Gourav Khator <gouravkhator9@gmail.com>
2019-12-25 13:17:08 +0530

    Finish Book Routes

--- models/book.js
+++ models/book.js
@@ -39,9 +39,9 @@ const bookSchema = mongoose.Schema({
 //This is virtual attribute definition like all other attributes which calls the function given 
 //It is used when we don't want to save this but require some manipulations on some defined attribute and access it directly
 
-bookSchema.virtual('coverImagePath', function () {
+bookSchema.virtual('coverImagePath').get(function () {
     if (this.coverImage != null && this.coverImageType != null) //if name of book cover file is set
-        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
+        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`;
 
     //in html to add src to img tag, data: can be used to display buffer data
     //data type and then charset and encoding then actual image to string from base64 format
